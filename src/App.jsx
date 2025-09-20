112cce5e8461dbd776af3ee73147899f121810c0
Mubaraq-Ay <ayanlekemubaraq@gmail.com>
2025-09-20 11:02:27 +0100

    pls work

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,14 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import Navbar from "./components/Navbar";
-import ViewAllJobs from "./components/ViewAllJobs";
+ import HomePage from './pages/HomePage';
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={ <HomePage /> } />)
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router}/> ;
 };
 
 export default App;
