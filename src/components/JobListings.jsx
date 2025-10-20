805d08bd6fd00312b0d72848a637904ed1877a13
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2025-10-19 17:25:14 -0700

    modified

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -12,7 +12,7 @@ const JobListings = ({ isHome = false }) => {
         const data = await res.json();
         setJobs(data);
       } catch (error) {
-          console.log('error fetching data', error);
+        console.log('error fetching data', error);
       } finally {
         setLoading(false);
       }
@@ -27,9 +27,17 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            <>
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+            </>
+          )
+          }
+
         </div>
       </div>
     </section>
