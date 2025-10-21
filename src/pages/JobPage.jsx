fe77d0dce486cff40e59b7c2067e334e27992e98
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2025-10-21 16:51:44 -0700

    commit

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,33 @@
-const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
-}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
-export default JobPage
+const JobPage = () => {
+    const { id } = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(`/api/job/${id}`);
+                const data = await res.json();
+                setJob(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJob();
+    }, []); // 
+
+    return loading ? <Spinner /> : (
+        <h1>Job</h1>
+    )
+
+
+};
+
+export default JobPage;
